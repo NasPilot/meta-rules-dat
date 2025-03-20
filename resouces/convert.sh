fa6672731725ef65dec47bcdd41e4f2e2b8a6d33
NasPilot <theaxn@gmail.com>
2025-03-20 23:18:31 +0800

    geo

--- resouces/convert.sh
+++ resouces/convert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 list=($(./sing-box geosite list | sed 's/ (.*)$//g'))
-mkdir -p geo-lite/geosite
+mkdir -p geo/geosite
 for ((i = 0; i < ${#list[@]}; i++)); do
 	if [[ "${list[i]}" == "category-pt" || "${list[i]}" == "category-pt@!cn" ]]; then
-		./sing-box geosite export ${list[i]} -o ./geo-lite/geosite/${list[i]}.json
-		./sing-box rule-set compile ./geo-lite/geosite/${list[i]}.json -o ./geo-lite/geosite/${list[i]}.srs
+		./sing-box geosite export ${list[i]} -o ./geo/geosite/${list[i]}.json
+		./sing-box rule-set compile ./geo/geosite/${list[i]}.json -o ./geo/geosite/${list[i]}.srs
 	fi
 done
